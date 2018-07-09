ef9fb54043fedd8a6ce176827ecf0f2d2d3fbf7f
mvalchanov <m.valchanov@abv.bg>
2018-07-09 11:32:03 +0300

    Working files

---
+++ XMLfromToCV/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using XMLfromToCV.Models;
+
+namespace XMLfromToCV.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IHostingEnvironment _env;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            this._env = env;
+        }
+
+        /// <summary>
+        /// Deserialize and load informationfrom current .xml file
+        /// </summary>
+        public IActionResult Index()
+        {
+            try
+            {
+                Person person = FindPersonDir("cv.xml");
+                return View(person);
+            }
+            catch (Exception ex)
+            {
+                //Logger
+                throw;
+            }
+        }
+
+        
+
+        /// <summary>
+        /// Deserialize and load informationfrom new .xml file
+        /// </summary>
+        public IActionResult IndexEdited()
+        {
+            try
+            {
+                Person person = FindPersonDir("cv-Edited.xml");
+                return View(person);
+            }
+            catch (Exception ex)
+            {
+                //Logger
+                throw;
+            }
+        }
+        /// <summary>
+        /// Return person from edit form and create/re-write
+        ///     .xml file
+        /// </summary>
+        /// <param name="person"> Person Information from edit form</param>
+        /// <returns>
+        /// View with edited information
+        /// </returns>
+
+        public IActionResult Edit(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", person);
+            }
+
+            string wwwroot = _env.WebRootPath;
+            string projectFolder = "\\xmls\\";
+            string parentDirName = string.Concat(wwwroot, projectFolder);
+            string fileName = "cv-Edited.xml";
+
+            
+            TextWriter writer = new StreamWriter(parentDirName + fileName);
+            XmlSerializer ser = new XmlSerializer(typeof(Person));
+
+            //Start writing to the cv-Edited.xml and create if needs.
+            ser.Serialize(writer, person);
+            writer.Close();
+
+            return RedirectToAction(nameof(IndexEdited));
+        }
+
+        /// <summary>
+        /// Localize \xmls\ directory
+        /// </summary>
+        /// <param name="fileName"></param>
+        private Person FindPersonDir(string fileName)
+        {
+
+            Person person;
+            string wwwroot = _env.WebRootPath;
+            string projectFolder = "\\xmls\\";
+            string parentDirName = string.Concat(wwwroot, projectFolder);
+
+
+            XmlSerializer des = new XmlSerializer(typeof(Person));
+            using (XmlReader reader = XmlReader.Create(parentDirName + fileName))
+            {
+                person = (Person)des.Deserialize(reader);
+            }
+
+            return person;
+        }
+    }
+}
---
+++ XMLfromToCV/Models/Person.cs
@@ -0,0 +1,129 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace XMLfromToCV.Models
+{
+
+    [XmlRoot("Person")]
+    public class Person
+    {
+        //Personal Information
+        [XmlElement("FullName")]
+        [Required(ErrorMessage = @"The ""Full Name"" field is required.")]
+        public string FullName { get; set; }
+        [XmlElement("Address")]
+        [Required(ErrorMessage = @"The ""Address"" field is required.")]
+        public string Address { get; set; }
+        [XmlElement("Phone")]
+        [Required(ErrorMessage = @"The ""Phone No."" field is required.")]
+        public string Phone { get; set; }
+
+        //See more about email regex at https://docs.microsoft.com/en-us/dotnet/standard/base-types/how-to-verify-that-strings-are-in-valid-email-format
+        //Regex editor https://regex101.com/
+        [RegularExpression((@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"), ErrorMessage = "Invalid e-mail address!")]
+        [Required(ErrorMessage = @"The ""E-mail"" field is required.")]
+        [XmlElement("E-mail")]
+        public string Email { get; set; }
+        [XmlElement("Nationality")]
+        public string Nationality { get; set; }
+        [XmlElement("DateOfBiirth")]
+        [Required(ErrorMessage = @"The ""Birth date"" field is required.")]
+        public string DateOfBirth { get; set; }
+
+        //Working experience
+        [XmlAttribute("WorkingExperience")]
+        public string WorkExp { get; set; }
+        [XmlElement("WorkDateFrom")]
+        public string WorkDateFrom { get; set; }
+        [XmlElement("WorkDateTo")]
+        public string WorkDateTo { get; set; }
+        [XmlElement("Employer")]
+        public string Employer { get; set; }
+        [XmlElement("WorkOccupation")]
+        public string WorkOccupation { get; set; }
+        [XmlElement("WorkMainActAndResp")]
+        public string WorkMainActAndResp { get; set; }
+
+        //Education
+        [XmlElement("UniversityName")]
+        public string UniversityName { get; set; }
+        [XmlElement("EdDateFrom")]
+        public string EdDateFrom { get; set; }
+        [XmlElement("EdDateTo")]
+        public string EdDateTo { get; set; }
+        [XmlElement("EdOrganization")]
+        public string EdOrganization { get; set; }
+        [XmlElement("EdQualification")]
+        public string EdQualification { get; set; }
+        [XmlElement("EdOccupation")]
+        public string EdOccupation { get; set; }
+
+        //Personal Skills and competences
+        [XmlElement("MotherLanguage")]
+        public string MotherLanguage { get; set; }
+        [XmlElement("Language")]
+        public string Language { get; set; }
+        [XmlElement("ReadingSkills")]
+        public string ReadingSkills { get; set; }
+        [XmlElement("WritingSkills")]
+        public string WritingSkills { get; set; }
+        [XmlElement("VerbalSkills")]
+        public string VerbalSkills { get; set; }
+
+        //Social skills
+        [XmlElement("SocSkill1")]
+        public string SocSkill1 { get; set; }
+        [XmlElement("SocSkill2")]
+        public string SocSkill2 { get; set; }
+        [XmlElement("SocSkill3")]
+        public string SocSkill3 { get; set; }
+        [XmlElement("SocSkill4")]
+        public string SocSkill4 { get; set; }
+        [XmlElement("SocSkill5")]
+        public string SocSkill5 { get; set; }
+        [XmlElement("SocSkill6")]
+        public string SocSkill6 { get; set; }
+
+        //Organization skills
+        [XmlElement("OrgSkill")]
+        public string OrgSkill { get; set; }
+        [XmlElement("OrgSkill1")]
+        public string OrgSkill1 { get; set; }
+        [XmlElement("OrgSkill2")]
+        public string OrgSkill2 { get; set; }
+        [XmlElement("OrgSkill3")]
+        public string OrgSkill3 { get; set; }
+
+        //Working skills
+        [XmlElement("WorkSkill")]
+        public string WorkSkill { get; set; }
+        [XmlElement("WorkSkill1")]
+        public string WorkSkill1 { get; set; }
+        [XmlElement("WorkSkill2")]
+        public string WorkSkill2 { get; set; }
+        [XmlElement("WorkSkill3")]
+        public string WorkSkill3 { get; set; }
+        [XmlElement("WorkSkill4")]
+        public string WorkSkill4 { get; set; }
+
+        //Basic working skills
+        [XmlElement("BasicSkill")]
+        public string BasicSkill { get; set; }
+        [XmlElement("BasicSkill1")]
+        public string BasicSkill1 { get; set; }
+        [XmlElement("BasicSkill2")]
+        public string BasicSkill2 { get; set; }
+        [XmlElement("BasicSkill3")]
+        public string BasicSkill3 { get; set; }
+
+        //Artistic skills
+        [XmlElement("ArtSkill")]
+        public string ArtSkill { get; set; }
+        [XmlElement("ArtSkill1")]
+        public string ArtSkill1 { get; set; }
+
+        //Additional information
+        [XmlElement("AdditionalInformation")]
+        public string AdditionalInformation { get; set; }
+    }
+}
